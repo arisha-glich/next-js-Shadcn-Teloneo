11b9a645feffdf42cbc5b73cda72a2bfc5ccb2b9
arisha <arishanawaz2002@gmail.com>
2024-09-13 17:01:01 +0500

    add

--- components/Reusables/Footer.tsx
+++ components/Reusables/Footer.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import SvgLogo from '../../public/logo.svg'
-//import logo from '../../public/logo.svg'; // Import the logo correctly
+import SvgLogo from '../../public/logo.svg';
 
 const Footer: React.FC = () => {
-  return (
-    <footer className="bg-primary w-[1349px] h-[135px] flex items-center justify-between px-8 mx-auto">
-      {/* Left Section: Logo */}
-      <div className="flex items-center">
-      <SvgLogo width={174} height={38} />
-      </div>
+ return (
+  <footer className="mx-auto flex h-[135px] w-[1349px] items-center justify-between bg-primary px-8">
+   {/* Left Section: Logo */}
+   <div className="flex items-center">
+    <SvgLogo width={174} height={38} />
+   </div>
+   {/* Center Section: Copyright */}
+   <div className="text-center text-xs font-normal text-secondary">
+    ©2023 Teleneo, LLC. All rights reserved.
+   </div>
 
-      {/* Center Section: Copyright */}
-      <div className="text-secondary text-xs font-normal text-center">
-        ©2023 Teleneo, LLC. All rights reserved.
-      </div>
-
-      {/* Right Section: Contact Us */}
-      <div>
-        <a
-          href="/contact"
-          className="text-secondary text-sm font-medium hover:underline"
-        >
-          Contact us
-        </a>
-      </div>
-    </footer>
-  );
+   {/* Right Section: Contact Us */}
+   <div>
+    <a
+     href="/contact"
+     className="text-sm font-medium text-secondary hover:underline"
+    >
+     Contact us
+    </a>
+   </div>
+  </footer>
+ );
 };
 
 export default Footer;
