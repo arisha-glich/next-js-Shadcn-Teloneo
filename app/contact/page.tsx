8c0f38a7b1035f1624e2f981b4960c790646ce15
arisha <arishanawaz2002@gmail.com>
2024-09-18 17:09:23 +0500

    contactus-page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,220 +11,197 @@ import {
 import { Button } from '@/components/ui/button';
 import Sidebar from '@/components/Reusables/SideBar';
 import Arrow from '../../public/arrow-right-solid 2.svg';
+import Icon from '../../public/cards/clipboard-list-check-solid 1.svg';
 
 // Sample data
 const contacts = [
- {
+    {
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
+        name: 'Jacob Santner',
+        email: 'abc.olivia@email.com',
+        addedOn: '01/12/2023',
+        query: 'I recently used this website to find a Doctor for my child...',
+       },{
   name: 'Jacob Santner',
   email: 'abc.olivia@email.com',
   addedOn: '01/12/2023',
   query: 'I recently used this website to find a Doctor for my child...',
- },
- {
+ },{
   name: 'Jacob Santner',
   email: 'abc.olivia@email.com',
   addedOn: '01/12/2023',
   query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- {
-  name: 'Jacob Santner',
-  email: 'abc.olivia@email.com',
-  addedOn: '01/12/2023',
-  query: 'I recently used this website to find a Doctor for my child...',
- },
- // ... your sample data
+ },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },{
+    name: 'Jacob Santner',
+    email: 'abc.olivia@email.com',
+    addedOn: '01/12/2023',
+    query: 'I recently used this website to find a Doctor for my child...',
+   },
+ 
 ];
 
 const ContactTable = () => {
@@ -243,29 +220,38 @@ const ContactTable = () => {
  const totalPages = Math.ceil(contacts.length / contactsPerPage);
 
  return (
-  <div className="flex min-h-screen">
+  <div className="flex">
    <Sidebar />
    <div className="flex-grow p-12 pr-10">
-    <div className="mx-auto ml-10 flex h-[581px] w-[1260px] flex-col gap-4 bg-ring pl-5">
-     <div className="overflow-x-auto">
-      <Table className="w-full">
+    <div className="mx-auto flex h-[100] w-[1128px] flex-col gap-4 pl-5">
+     <header className="mb-4 flex items-center justify-between">
+      <h1 className="text-2xl font-semibold">Contact Us</h1>
+      <Button variant="default" className="flex items-center space-x-2">
+       <Icon className="h-5 w-5" /> {/* Adjust size as needed */}
+       <span>Waitlist</span>
+      </Button>
+     </header>
+    </div>
+    <div className="mx-auto border-gray-300 border-t-2 ml-10 flex h-[581px] w-[1260px] font-sans text-[12px] text-secondary-foreground flex-col gap-4 bg-ring pl-5">
+     <div className="overflow-x-hidden">
+      <Table className="w-full border-collapse">
        <TableHeader>
-        <TableRow>
-         <TableHead>Name</TableHead>
-         <TableHead>Email address</TableHead>
-         <TableHead>Added on</TableHead>
-         <TableHead>Queries</TableHead>
-         <TableHead>Action</TableHead>
+        <TableRow className="border-b-0">
+         <TableHead className="border-b-0">Name</TableHead>
+         <TableHead className="border-b-0">Email address</TableHead>
+         <TableHead className="border-b-0">Added on</TableHead>
+         <TableHead className="border-b-0">Queries</TableHead>
+         <TableHead className="border-b-0">Action</TableHead>
         </TableRow>
        </TableHeader>
        <TableBody>
         {currentContacts.map((contact, index) => (
-         <TableRow key={index}>
-          <TableCell>{contact.name}</TableCell>
-          <TableCell>{contact.email}</TableCell>
-          <TableCell>{contact.addedOn}</TableCell>
-          <TableCell>{contact.query}</TableCell>
-          <TableCell>
+         <TableRow className='text-[13px] font-sans text-black border-b-0' key={index}>
+          <TableCell className="border-b-0">{contact.name}</TableCell>
+          <TableCell className="border-b-0">{contact.email}</TableCell>
+          <TableCell className="border-b-0">{contact.addedOn}</TableCell>
+          <TableCell className="border-b-0">{contact.query}</TableCell>
+          <TableCell className="border-b-0">
            <Button variant="link" className="text-primary">
             View
            </Button>
@@ -276,8 +262,8 @@ const ContactTable = () => {
       </Table>
      </div>
 
-     {/* Pagination */}
-     <div className="mt-4 flex items-center justify-between">
+  {/* Pagination */}
+  <div className="mt-4 flex items-center justify-between">
       <div className="flex gap-2">
        {[...Array(totalPages)].map((_, i) => (
         <Button
