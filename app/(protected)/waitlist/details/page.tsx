3bae7088ef8f3600bf21fdead11b75cfaa03a8a4
arisha <arishanawaz2002@gmail.com>
2024-09-25 22:09:42 +0500

    use-State issue

--- app/(protected)/waitlist/details/page.tsx
+++ app/(protected)/waitlist/details/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { useState } from 'react';
 import { ArrowLeft, VideoIcon } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent } from '@/components/ui/card';
@@ -12,10 +11,9 @@ import Sidebar from '@/components/Reusables/SideBar';
 import Avataricon from '@/public/patient/60111 [Converted]-01 2.svg';
 import Videoicon from '@/public/patient/video-light 1.svg';
 import Link from 'next/link';
+import Icon from '@/public/cards/clipboard-list-check-solid 1.svg';
 
 export default function AppointmentDetails() {
- const [activeTab, setActiveTab] = useState('parent');
-
  return (
   <div className="flex h-screen p-0">
    <div className="w-[88px]">
@@ -32,30 +30,23 @@ export default function AppointmentDetails() {
        </Link>
        <h1 className="text-2xl font-bold">Waitlist Details</h1>
       </div>
-      <Button variant="default" className="hover:primary bg-primary text-white">
-       Waitlist
+      <Button variant="default" className="flex items-center space-x-2">
+       <Icon className="h-5 w-5" />
+       <span>Waitlist</span>
       </Button>
      </header>
 
-     <Tabs value={activeTab} onValueChange={setActiveTab} className="space-y-1">
+     <Tabs defaultValue="parent" className="space-y-1">
       <TabsList className="grid w-[calc(100%-100px)] grid-cols-6 bg-transparent p-0">
        <TabsTrigger
         value="parent"
-        className={`rounded-t-lg border-b-2 px-4 py-2 ${
-         activeTab === 'parent'
-          ? 'border-orange-500 text-accent-foreground'
-          : 'border-transparent'
-        }`}
+        className="rounded-t-lg border-b-2 border-orange-500 px-4 py-2 text-accent-foreground"
        >
         Parent / Guardian
        </TabsTrigger>
        <TabsTrigger
         value="patient"
-        className={`rounded-t-lg border-b-2 px-4 py-2 ${
-         activeTab === 'patient'
-          ? 'border-accent-foreground text-accent-foreground'
-          : 'border-transparent'
-        }`}
+        className="rounded-t-lg border-b-2 border-transparent px-4 py-2"
        >
         Patient Info
        </TabsTrigger>
@@ -68,7 +59,6 @@ export default function AppointmentDetails() {
            <Avatar className="h-100 w-45 ml-6 rounded-none">
             <Avataricon className="h-130 w-130 mr-2 border-none" />
            </Avatar>
-
            <div>
             <h2 className="text-xl font-semibold">Marry May</h2>
             <p className="text-sm text-gray-500">Parent</p>
@@ -84,8 +74,7 @@ export default function AppointmentDetails() {
             Type
            </Label>
            <div className="w-400 flex items-center">
-            <Videoicon className="mr-2 h-7 w-10 text-gray-500" />{' '}
-            {/* Added padding and margin */}
+            <Videoicon className="mr-2 h-7 w-10 text-gray-500" />
             <Input
              type="text"
              id="Video Appointment"
@@ -98,14 +87,14 @@ export default function AppointmentDetails() {
 
           <div className="flex h-[45px] w-[546px] items-center rounded-md border border-gray-200 bg-gray-100 px-4">
            <Label
-            htmlFor="text"
+            htmlFor="timeSlot"
             className="w-1/2 text-[13px] font-bold text-secondary-foreground"
            >
             Time slot booked
            </Label>
            <Input
-            type="email"
-            id="email"
+            type="text"
+            id="timeSlot"
             value="14/2/2023 | 6:00 PM EST"
             readOnly
             className="w-400 border-none p-0 text-[13px] text-gray-900 shadow-none focus:ring-0"
@@ -145,15 +134,15 @@ export default function AppointmentDetails() {
           <div>
            <div className="flex h-[45px] w-[546px] items-center rounded-md border border-gray-200 bg-gray-100 px-4">
             <Label
-             htmlFor="email"
+             htmlFor="fullName"
              className="w-1/2 text-[13px] font-bold text-secondary-foreground"
             >
              Full Name
             </Label>
             <Input
-             type="email"
-             id="email"
-             value="johny Star"
+             type="text"
+             id="fullName"
+             value="Johnny Star"
              readOnly
              className="w-400 border-none p-0 text-[13px] text-gray-900 shadow-none focus:ring-0"
             />
@@ -161,14 +150,14 @@ export default function AppointmentDetails() {
           </div>
           <div className="flex h-[45px] w-[546px] items-center rounded-md border border-gray-200 bg-gray-100 px-4">
            <Label
-            htmlFor="email"
+            htmlFor="age"
             className="w-1/2 text-[13px] font-bold text-secondary-foreground"
            >
             Age
            </Label>
            <Input
-            type="email"
-            id="email"
+            type="text"
+            id="age"
             value="16"
             readOnly
             className="w-400 border-none p-0 text-[13px] text-gray-900 shadow-none focus:ring-0"
@@ -176,14 +165,14 @@ export default function AppointmentDetails() {
           </div>
           <div className="flex h-[45px] w-[546px] items-center rounded-md border border-gray-200 bg-gray-100 px-4">
            <Label
-            htmlFor="email"
+            htmlFor="gender"
             className="w-1/2 text-[13px] font-bold text-secondary-foreground"
            >
             Gender
            </Label>
            <Input
-            type="email"
-            id="email"
+            type="text"
+            id="gender"
             value="Female"
             readOnly
             className="w-400 border-none p-0 text-[13px] text-gray-900 shadow-none focus:ring-0"
@@ -191,14 +180,14 @@ export default function AppointmentDetails() {
           </div>
           <div className="flex h-[45px] w-[546px] items-center rounded-md border border-gray-200 bg-gray-100 px-4">
            <Label
-            htmlFor="email"
+            htmlFor="psychCondition"
             className="w-1/2 text-[13px] font-bold text-secondary-foreground"
            >
             Psychological condition
            </Label>
            <Input
-            type="email"
-            id="email"
+            type="text"
+            id="psychCondition"
             value="Stress"
             readOnly
             className="w-400 border-none p-0 text-[13px] text-gray-900 shadow-none focus:ring-0"
